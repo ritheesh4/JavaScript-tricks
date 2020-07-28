3597c58e14bc0d2dd82e4af095398829f3b59c40
ritheesh4 <ritheesh.4@gmail.com>
2020-07-28 21:58:59 +0530

    ternary operator

---
+++ index.js
@@ -0,0 +1,7 @@
+const age = 10;
+
+age > 50
+    ? age> 70
+        ? console.log("You are getting really old")
+        : console.log("You are between 30 and 59")
+    : console.log("You are below 30");
