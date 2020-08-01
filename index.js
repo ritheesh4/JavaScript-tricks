9f28000804fb1dae64a637199c39448b09fad3fd
ritheesh4 <ritheesh.4@gmail.com>
2020-08-01 22:02:07 +0530

    merge array objects tricks added

--- index.js
+++ index.js
@@ -62,3 +62,10 @@ array = array.slice(0, 4);console.log(array); // Result: [0, 1, 2, 3]
 let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];console.log(array.slice(-1)); // Result: [9]
 console.log(array.slice(-2)); // Result: [8, 9]
 console.log(array.slice(-3)); // Result: [7, 8, 9]
+
+// Merge array elements
+const person = { name: 'David Walsh', gender: 'Male' };
+const tools = { computer: 'Mac', editor: 'Atom' };
+const attributes = { handsomeness: 'Extreme', hair: 'Brown', eyes: 'Blue' };
+
+const summary = {...person, ...tools, ...attributes};
