dba4e11272a181e21534d3d45de2328f3f3c8fdf
ritheesh4 <ritheesh.4@gmail.com>
2020-08-07 08:27:31 +0530

    array falsy value filtering and merge objects trciks added

--- index.js
+++ index.js
@@ -27,63 +27,64 @@ hello(undefined);
 hello(null);
 hello("David");
 
-
 //3. Type conversion
 
 let int = "15";
-int = +int;console.log(int); // Result: 15
-console.log(typeof int); 
+int = +int;
+console.log(int); // Result: 15
+console.log(typeof int);
 
 //4. Quick float to integer conversion. Without usin Math.ceil() or Math.round().
 
-console.log(23.9 | 0);  // Result: 23
+console.log(23.9 | 0); // Result: 23
 console.log(-23.9 | 0); // Result: -23
 
 //5.  Remove Final Digits
-let str = "1553"; 
+let str = "1553";
 Number(str.substring(0, str.length - 1)); // Instead of using this, below method can be used.
 
-console.log(1553 / 10   | 0)  // Result: 155
-console.log(1553 / 100  | 0)  // Result: 15
-console.log(1553 / 1000 | 0)  // Result: 1
+console.log((1553 / 10) | 0); // Result: 155
+console.log((1553 / 100) | 0); // Result: 15
+console.log((1553 / 1000) | 0); // Result: 1
 
 //6. Easiest way to remove last elements from array or truncate an array without using splice().
 
 let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-array.length = 4;console.log(array); // Result: [0, 1, 2, 3];
+array.length = 4;
+console.log(array); // Result: [0, 1, 2, 3];
 
 //7. If speed is more important to t remove the array elements use splice
 let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-array = array.slice(0, 4);console.log(array); // Result: [0, 1, 2, 3]
-
+array = array.slice(0, 4);
+console.log(array); // Result: [0, 1, 2, 3]
 
 //8. Easiest way to get the last elements in an array
 
-let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];console.log(array.slice(-1)); // Result: [9]
+let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(array.slice(-1)); // Result: [9]
 console.log(array.slice(-2)); // Result: [8, 9]
 console.log(array.slice(-3)); // Result: [7, 8, 9]
 
 //9. Merge array elements
-const person = { name: 'David Walsh', gender: 'Male' };
-const tools = { computer: 'Mac', editor: 'Atom' };
-const attributes = { handsomeness: 'Extreme', hair: 'Brown', eyes: 'Blue' };
+const person = { name: "David Walsh", gender: "Male" };
+const tools = { computer: "Mac", editor: "Atom" };
+const attributes = { handsomeness: "Extreme", hair: "Brown", eyes: "Blue" };
 
-const summary = {...person, ...tools, ...attributes};
+const summary = { ...person, ...tools, ...attributes };
 
 //10. Self calling function
-(function(){
+(function () {
   // some private code that will be executed automatically
-})();  
-(function(a,b){
-  var result = a+b;
+})();
+(function (a, b) {
+  var result = a + b;
   return result;
-})(10,20)
-
+})(10, 20);
 
 //11. FIlter unique values
 // The Set object type was introduced in ES6, and along with ..., the ‘spread’ operator, we can use it to create a new array with only the unique values.
 
-const array = [1, 1, 2, 3, 5, 5, 1]
+const array = [1, 1, 2, 3, 5, 5, 1];
 const uniqueArray = [...new Set(array)];
 console.log(uniqueArray); // Result: [1, 2, 3, 5]
 
@@ -92,10 +93,30 @@ console.log(uniqueArray); // Result: [1, 2, 3, 5]
 
 //12. Float to Integer
 
-console.log(23.9 | 0);  // Result: 23
+console.log(23.9 | 0); // Result: 23
 console.log(-23.9 | 0); // Result: -23
 
 //13. Last items in an array
-let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];console.log(array.slice(-1)); // Result: [9]
+let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(array.slice(-1)); // Result: [9]
 console.log(array.slice(-2)); // Result: [8, 9]
 console.log(array.slice(-3)); // Result: [7, 8, 9]
+
+//14. Easier way to fitler out the falsy values from an array
+myArray.filter(Boolean);
+
+//15. Merge several objects together
+const user = {
+  name: "John Ludwig",
+  gender: "Male",
+};
+const college = {
+  primary: "Mani Primary School",
+  secondary: "Lass Secondary School",
+};
+const skills = {
+  programming: "Extreme",
+  swimming: "Average",
+  sleeping: "Pro",
+};
+const summary = { ...user, ...college, ...skills };
