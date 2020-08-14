e2c2a1e957b6d271cc6d6e33e306f9a704b03805
ritheesh4 <ritheesh.4@gmail.com>
2020-08-14 23:31:08 +0530

    query string parameters

--- index.js
+++ index.js
@@ -186,3 +186,11 @@ let arrayElements = [].slice.call(elements); // Now the NodeList is an array
 
 let arrayElements = Array.from(elements); // This is another way of converting NodeList to Array
 
+
+let urlParams = new URLSearchParams(window.location.search);
+
+console.log(urlParams.has('post')); // true
+console.log(urlParams.get('action')); // "edit"
+console.log(urlParams.getAll('action')); // ["edit"]
+console.log(urlParams.toString()); // "?post=1234&action=edit"
+console.log(urlParams.append('active', '1')); // "?post=1234&action=edit&active=1"
