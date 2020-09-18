49824ec912954175df6c385b81c01ed488af50cc
ritheesh4 <ritheesh.4@gmail.com>
2020-09-18 22:48:32 +0530

    shuffle array elements trick added

--- index.js
+++ index.js
@@ -570,3 +570,10 @@ Promise.all(PromiseArray)
 Math.pow(2, n);
 2 << (n - 1);
 2**n;
+
+// 73. Shuffle elements in an array
+let list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(list.sort(function() {
+    return Math.random() - 0.5
+})); 
+// [4, 8, 2, 9, 1, 3, 6, 5, 7]
