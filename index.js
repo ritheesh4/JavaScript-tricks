94448c2975fbe850f651f8548d5440c748ac1bde
ritheesh4 <ritheesh.4@gmail.com>
2020-09-19 21:51:24 +0530

    required parameter trick added

--- index.js
+++ index.js
@@ -584,3 +584,17 @@ const tools = { computer: 'Mac', editor: 'Atom' };
 const attributes = { handsomeness: 'Extreme', hair: 'Brown', eyes: 'Blue' };
 
 const summary = {...person, ...tools, ...attributes};
+
+// 75. required parameter
+const isRequired = () => { throw new Error('param is required'); };
+
+const hello = (name = isRequired()) => { console.log(`hello ${name}`) };
+
+// This will throw an error because no name is provided
+hello();
+
+// This will also throw an error
+hello(undefined);
+
+// These are good!
+hello(null);
