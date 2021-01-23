e7691eff347dbd7a12956e4a8bb729f7e5769723
Ritheesh <ritheesh.4@gmail.com>
2021-01-23 22:06:22 +0530

    convert values' types

--- index.js
+++ index.js
@@ -1044,3 +1044,54 @@ const { x } = obj;
 
 // Grabs obj.x as { otherName }
 const { x: otherName } = obj;
+
+// 127 Convert values to  types
+// From anything to a number
+
+var foo = "42";
+var myNumber = +foo; // shortcut for Number(foo)
+// → 42
+
+// Tip: you can convert it directly into a negative number
+var negativeFoo = -foo; // or -Number(foo)
+// → -42
+
+// From object to array
+// Tip: `arguments` is an object and in general you want to use it as array
+var args = { 0: "foo", 1: "bar", length: 2 };
+Array.prototype.slice.call(args)
+// → [ 'foo', 'bar' ]
+
+// Anything to boolean
+/// Non non p is a boolean p
+var t = 1;
+var f = 0;
+!!t
+// → true
+!!f
+// → false
+
+/// And non-p is a boolean non-p
+!t
+// → false
+!f
+// → true
+
+// Anything to string
+var foo = 42;
+"" + foo // shortcut for String(foo)
+// → "42"
+
+foo = { hello: "world" };
+JSON.stringify(foo);
+// → '{ "hello":"world" }'
+
+JSON.stringify(foo, null, 4); // beautify the things
+// →
+// '{
+//    "hello": "world"
+// }'
+
+// Note you cannot JSON.stringify circular structures
+JSON.stringify(window);
+// ⚠ TypeError: JSON.stringify cannot serialize cyclic structures.
